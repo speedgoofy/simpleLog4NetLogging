95d52e4af28e713ab776795fc61fb87c608c1323
Brandon <speedgoofy@gmail.com>
2017-01-13 13:27:14 -0600

    First Commit

---
+++ Src/Log4NetStart/LogHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Log4NetStart
+{
+    class LogHelper
+    {
+        public static log4net.ILog GetLogger([CallerFilePath]string filename = "")
+        {
+            return log4net.LogManager.GetLogger(filename);
+        }
+    }
+}
---
+++ Src/Log4NetStart/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// this is to watch the config file for 
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace Log4NetStart
+{
+    class Program
+    {
+        // this tells what class you are in when the logging accuared 
+        private static readonly log4net.ILog log = LogHelper.GetLogger(); //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            
+        static void Main(string[] args)
+        {
+            log.Debug("Developer: Tuturial run");
+            log.Info("Oil change needed");
+            log.Warn("Car is geting hot");
+
+            var i = 0;
+            var str = "hello my name is brandon";
+
+            log.Info(str);
+
+            try
+            {
+                var x = 10 / i;
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                log.Error("Developer, We have a problem !! ", ex);
+            }
+
+            log.Fatal("Water Pump Exploded!!! ");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
